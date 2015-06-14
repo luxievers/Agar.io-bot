696a8dee30265789cb4933d2b4908bb7d9546803
luxievers <luxi@kevers.dk>
2015-06-14 12:45:22 -0700

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -884,7 +884,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  originalName = "NotReallyABot",
+  originalName = "Denmark",
   sessionScore = 0,
   serverIP = "",
   interNodes = [],
